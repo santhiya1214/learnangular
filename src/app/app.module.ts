24a1339d95601536b2816f62eae2e6973bee6658
santhiya1214 <[santhiyajawahar@gmail.com]>
2022-04-20 09:04:51 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+//import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warningalert/warning.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { BindingsComponent } from './bindings/bindings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+   // ServersComponent,
+    WarningComponent,
+    SuccessAlertComponent,
+    BindingsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/warningalert/warning.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector:'app-warning',
+   //templateUrl:'./warning.component.html',
+    template : `<p> You got a warning</p>`,
+    styles: [`p {color:blueviolet}`]
+  }
+)
+export class WarningComponent{
+
+}
